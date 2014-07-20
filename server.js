cfca89fb01eecd034aa4bfcb6d1684c2645c89ee
Erick Alvarez <e_adrian90@hotmail.com>
2014-07-20 15:22:48 -0500

    updating dependencies

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var fs = require('fs');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
 
 app.use('/', express.static(__dirname));
-app.use(express.bodyParser());
+
+// parse urlencoded request bodies into req.body
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/comments.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
